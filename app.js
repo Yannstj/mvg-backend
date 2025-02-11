ea0dc65bf10169bfe75875403c7f82b9b44d4cb9
Yannstj <yannick.saintjust@gmail.com>
2025-02-11 20:22:28 +0100

    config prettier and linter

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 require('dotenv').config();
 
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const mongoURI = process.env.MONGO_URI;
 const app = express();
 
-mongoose.connect(mongoURI,
-    { useNewUrlParser: true,
-      useUnifiedTopology: true })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'))
-
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((err) => {
+    console.log('Connexion à MongoDB échouée !', err);
+  });
 
 app.use((req, res) => {
-    res.json({message: 'Hello World !!'})
+  res.json({ message: 'Hello World !!' });
 });
 
 module.exports = app;
-
-
