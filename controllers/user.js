3660cd292e6667e43795e11ee9657451567e765f
Yannstj <yannick.saintjust@gmail.com>
2025-02-12 17:02:58 +0100

    auth ok middleware setted

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
   bcrypt
@@ -11,10 +12,36 @@ exports.signup = (req, res, next) => {
       });
       user
         .save()
-        .then(res.status(201).json({ message: 'Utilisateur crée' }))
+        .then(() => res.status(201).json({ message: 'Utilisateur crée' }))
         .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: 'Paire login/mot de passe incorrecte' });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res
+              .status(401)
+              .json({ message: 'Paire login/mot de passe incorrecte' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', {
+              expiresIn: '24h',
+            }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
