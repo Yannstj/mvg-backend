fdc9003a773cd31c909c6810dbc7701d7578c43e
Yannstj <yannick.saintjust@gmail.com>
2025-03-06 15:58:08 +0100

    caps for env variable naming convention

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
 require('dotenv').config;
 const jwt = require('jsonwebtoken');
-const secret = process.env.SECRET_TOKEN;
+const SECRET_TOKEN = process.env.SECRET_TOKEN;
 
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, secret);
+    const decodedToken = jwt.verify(token, SECRET_TOKEN);
     const userId = decodedToken.userId;
     req.auth = {
       userId: userId,
